a34bed2bd47e08b04ae683d0a8e91e0a09378c0c
ckendrick-mqu <christopher.kendrick1@students.mq.edu.au>
2026-08-18 12:45:02 +1000

    Create Cell.java

---
+++ Cell.java
@@ -0,0 +1,25 @@
+class Cell {
+    private int x;
+    private int y;
+    // tree object or lackthereof
+
+    Cell() {
+
+    }
+    
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int newX) {
+        x = newX;
+    }
+
+    public void setY(int newY) {
+        y = newY;
+    }
+}
