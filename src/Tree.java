0a7aca43f8d1bac8f34cbc7a51e30a179835f5cb
Gabriel <hackeduser777@gmail.com>
2026-08-18 12:40:40 +1000

    Added tree class code

---
+++ src/Tree.java
@@ -0,0 +1,13 @@
+public class Tree {
+    public boolean isBurning;
+    public double spreadability; 
+
+    public Tree(double spreadability){
+        this.spreadability = spreadability; 
+        this.isBurning = false; 
+    }
+
+    public void update(){
+        // fire spread logic can go here
+    }
+}
