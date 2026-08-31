628a71ad710b4d6b0f1b3374df2782b4a0e7e16d
Lolowa A <Lolowa-1@hotmail.com>
2026-09-01 03:38:47 +1000

    Add validated ignition source severity

--- src/IgnitionSource.java
+++ src/IgnitionSource.java
@@ -2,16 +2,24 @@ public class IgnitionSource {
     private int severity;
 
     public IgnitionSource() {
+       this(1);
+    }
 
-
+    public IgnitionSource(int severity) {
+        setSeverity(severity);
     }
 
     public int getSeverity() {
         return this.severity;
     }
 
-    public int setSeverity() {
-        return this.severity;
-    }
+    public void setSeverity(int severity) {
+        if (severity < 1 || severity > 5) {
+            throw new IllegalArgumentException(
+                    "Severity must be between 1 and 5"
+            );
+        }
 
+        this.severity = severity;
+    }
 }
