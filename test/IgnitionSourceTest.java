44a711cc07de5bc93e184e9e0583144ec344e4b1
Lolowa A <Lolowa-1@hotmail.com>
2026-09-06 20:30:59 +1000

    Add polymorphism test for ignition sources

--- test/IgnitionSourceTest.java
+++ test/IgnitionSourceTest.java
@@ -9,6 +9,7 @@ public class IgnitionSourceTest {
         testBackburningIgnitesWholeRow();
         testRejectsInvalidSeverity();
         testRejectsInvalidSpreadability();
+        testIgnitionSourcePolymorphism();
         
         System.out.println("All IgnitionSource tests passed.");
     }
@@ -95,6 +96,28 @@ public class IgnitionSourceTest {
             );
         }
     }
+
+private static void testIgnitionSourcePolymorphism() {
+    IgnitionSource[] sources = {
+            new Arson(0, 0),
+            new Lightning(),
+            new Backburning(0)
+    };
+
+    for (IgnitionSource source : sources) {
+        Forest forest = new Forest(1, 1);
+        forest.getGrid()[0][0].plantTree(new Tree(0.9));
+
+        source.ignite(forest);
+
+        check(
+                forest.getGrid()[0][0].getTree().isBurning(),
+                source.getClass().getSimpleName()
+                        + " should ignite through an IgnitionSource reference"
+        );
+    }
+}
+
 private static void testRejectsInvalidSeverity() {
     boolean exceptionThrown = false;
 
