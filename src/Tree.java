d5e4e02807edfb61939383863685b8d2d63643c4
ckendrick-mqu <christopher.kendrick1@students.mq.edu.au>
2026-09-12 03:45:47 +1000

    add tree health framework

--- src/Tree.java
+++ src/Tree.java
@@ -13,19 +13,28 @@ public class Tree implements Burnable {
     private double spreadability;
     private TreeType treeType;
     private double burnIntensity;
-    private int ignitionCount;
     private boolean alive;
+    private double health;
+    private double maxHealth;
+    private double burnIntensityFactor;
 
     public Tree(double spreadability) {
-        this(TreeType.OAK, spreadability);
+        this(TreeType.OAK, spreadability, 100.0, 0.75);
     }
 
-    public Tree(TreeType treeType, double spreadability) {
+    protected Tree(
+            TreeType treeType,
+            double spreadability,
+            double maxHealth,
+            double burnIntensityFactor) {
+
         this.burning = false;
         this.spreadability = spreadability;
         this.treeType = treeType;
         this.burnIntensity = 0.0;
-        this.ignitionCount = 0;
+        this.burnIntensityFactor = burnIntensityFactor;
+        this.maxHealth = maxHealth;
+        this.health = maxHealth;
         this.alive = true;
     }
 
@@ -35,10 +44,6 @@ public class Tree implements Burnable {
             return;
         }
 
-        if (!burning) {
-            ignitionCount++;
-        }
-
         burning = true;
         burnIntensity = 1.0;
     }
@@ -46,12 +51,28 @@ public class Tree implements Burnable {
     // OVERLOADED version
     public void ignite(double severity) {
         if (alive && severity >= 0.5) {
-            if (!burning) {
-                ignitionCount++;
-            }
-
             burning = true;
             burnIntensity = Math.max(burnIntensity, severity);
+        }
+    }
+
+    public double getHealth() {
+        return health;
+    }
+
+    public double getMaxHealth() {
+        return maxHealth;
+    }
+
+    public void damage(double amount) {
+        if (!alive) {
+            return;
+        }
+
+        health = Math.max(0.0, health - amount);
+
+        if (health == 0.0) {
+            kill();
         }
     }
 
@@ -73,10 +94,6 @@ public class Tree implements Burnable {
 
     public double getBurnIntensity() {
         return burnIntensity;
-    }
-
-    public int getIgnitionCount() {
-        return ignitionCount;
     }
 
     public void extinguish() {
